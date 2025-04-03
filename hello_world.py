f4acb853242a9ce6ed4a12a3f776c29ae57a77b6
Dmitriy <savin.dmitriy2812@gmail.com>
2025-04-03 08:04:21 +0300

    get request

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,7 @@
-print('Hello from Dmitriy')
+#print('Hello from Dmitriy')
+from requests import get
+
+url = 'https://playground.learnqa.ru/api/get_text'
+
+response = get(url)
+print(response.text)
